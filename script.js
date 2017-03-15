6020bc7b015737acccbed3d3448d9cb6530e92fb
Sabirproger <sabir77000@gmail.com>
2017-03-15 18:05:40 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var a=0, b=0, c=0, d=0, e=0;
+
+function sec1() {
+a++;
+if(a%2==1) {
+document.getElementsByClassName("text")[0].style.display="block";}
+else {
+document.getElementsByClassName("text")[0].style.display="none";
+}
+}
+
+function sec2() {
+b++;
+if(b%2==1) {
+document.getElementsByClassName("text")[1].style.display="block";}
+else {
+document.getElementsByClassName("text")[1].style.display="none";
+}
+}
+
+function sec3() {
+c++;
+if(c%2==1) {
+document.getElementsByClassName("text")[2].style.display="block";}
+else {
+document.getElementsByClassName("text")[2].style.display="none";
+}
+}
+
+function sec4() {
+d++;
+if(d%2==1) {
+document.getElementsByClassName("text")[3].style.display="block";}
+else {
+document.getElementsByClassName("text")[3].style.display="none";
+}
+}
+
+function sec5() {
+e++;
+if(e%2==1) {
+document.getElementsByClassName("text")[4].style.display="block";}
+else {
+document.getElementsByClassName("text")[4].style.display="none";
+}
+}
+
+
+
+
+
+var n=0, m=0, z=0, p=0, h=0;
+if(n==0){ function button() {
+document.getElementsByClassName("text")[0].style.display="block";
+}
+}
+
+if(m==0){ function button() {
+document.getElementsByClassName("text")[1].style.display="block";
+}
+}
+
